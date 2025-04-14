e4871560641d22db3df2430a5c73d7bedeaba793
RahulKavuri <rahultejakavuri@gmail.com>
2025-04-14 20:57:39 +0530

    iedf

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const productController = require("../controllers/productController");
 
 // Route to add a new product
-router.post("/api/products", productController.addProduct);
+router.post("api/products", productController.addProduct);
 
 
-router.get("/api/products", productController.getProducts)
+router.get("api/products", productController.getProducts)
 
 // ✅ Route to get all products or search by name using query param
-router.get("/api/products/search", productController.searchProducts);
+router.get("api/products/search", productController.searchProducts);
 
 module.exports = router;
